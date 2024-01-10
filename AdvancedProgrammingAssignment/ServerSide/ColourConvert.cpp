0bbace4ea75b52f4b74862afb24eced2eabd6982
Jonathan Byrne <jon.byrne27@btinternet.com>
2024-01-10 20:08:10 +0000

    Greyscale non-threaded working

--- AdvancedProgrammingAssignment/ServerSide/ColourConvert.cpp
+++ AdvancedProgrammingAssignment/ServerSide/ColourConvert.cpp
@@ -6,24 +6,28 @@ cv::Mat ColourConvert::process() {
     if (filterType == "greyscale") {
         int rows = adjustedImage.rows; //each row will be threaded individually
         vector<thread> threads;
-        /*
+        
         for (int i = 0; i < rows; i++) {
             threads.push_back(thread([&]() { //[&] captures everything used by the class, including variables i and adjustedImage
                 for (int j = 0; j < adjustedImage.cols; j++) {
-                    cv::Vec3b bgrVal = adjustedImage.at<float>(i, j); //gets the RGB value at row i and col j, however it is also realised that this is in BGR
+                    cv::Vec3b bgrVal = adjustedImage.at<cv::Vec3b>(i, j); //gets the RGB value at row i and col j, however it is also realised that this is in BGR
                     double blue = bgrVal[0];
                     double green = bgrVal[1];
                     double red = bgrVal[2];
                     double greyVal = (0.114 * blue) + (0.587 * green) + (0.299 * red); //combined value for greyscale
-                    adjustedImage.at<float>(i, j) = greyVal; //overwrites the current value to be the greyScale value
+                    cv::Vec3b greyPixel;
+                    greyPixel[0] = greyVal;
+                    greyPixel[1] = greyVal;
+                    greyPixel[2] = greyVal;
+                    adjustedImage.at<cv::Vec3b>(i, j) = greyPixel; //overwrites the current value to be the greyScale value
                 }
                 }));
         }
         for (auto& thread : threads) {
             thread.join();
         }
-        */
-        greyScale();
+        
+        //greyScale();
     }
     //if (filterType == "greyscale") greyScale();
     else {
@@ -38,23 +42,20 @@ void ColourConvert::greyScale() {
 */
 
 void ColourConvert::greyScale() {
-    for(int i = 0; i < image.cols; i++) {
-        for(int j = 0; j < image.cols; j++) {
-            cv::Vec3b bgrVal = image.at<cv::Vec3b>(i, j); //gets the RGB value at row i and col j, however it is also realised that this is in BGR
+    int counter = 0;
+    for(int i = 0; i < adjustedImage.rows; i++) {
+        for(int j = 0; j < adjustedImage.cols; j++) {
+            cv::Vec3b bgrVal = adjustedImage.at<cv::Vec3b>(i, j); //gets the RGB value at row i and col j, however it is also realised that this is in BGR
             double blue = bgrVal[0];
             double green = bgrVal[1];
             double red = bgrVal[2];
-            cv::Vec3b greyVal = (0.114 * blue) + (0.587 * green) + (0.299 * red); //combined value for greyscale
-            adjustedImage.at<cv::Vec3b>(i, j) = greyVal; //overwrites the current value to be the greyScale value
+            double greyVal = (0.114 * blue) + (0.587 * green) + (0.299 * red); //combined value for greyscale
+            cv::Vec3b greyPixel;
+            greyPixel[0] = greyVal;
+            greyPixel[1] = greyVal;
+            greyPixel[2] = greyVal;
+            adjustedImage.at<cv::Vec3b>(i, j) = greyPixel; //overwrites the current value to be the greyScale value
         }
     }
-    /*
-    lock
-    loop through
-    for (img)
-        img.at
-
-    unlock
-    */
     return;
 }
